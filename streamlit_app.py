1181eb8cf1e4d7155b0ec6a0419af9d48a4c9c26
lautarogarcia94 <lautygcr@gmail.com>
2022-06-14 11:14:40 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
+import streamlit
 
+streamlit.title('Breakfast Favorites')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Advocado Toast')
